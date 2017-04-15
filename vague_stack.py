fb39e1c9ac6db4bb585ea3c1376bd334dc1296ec
Utkarsh Chauhan <utkarshmail2052@gmail.com>
2017-04-16 00:18:37 +0530

    Create vague_stack.py
    Un tested. Only functions.

---
+++ vague_stack.py
@@ -0,0 +1,16 @@
+
+        
+class Stack:
+    def __init__(self):
+        self.stack = []
+   
+    def push(data):
+        self.stack = self.stack + [data]
+    
+    def pop(data):
+        if len(self.stack) > 0
+            popped = self.stack[len(self.stack)-1]
+            self.stack = self.stack[0:len(self.stack)-1]
+            return popped
+        return None
+
